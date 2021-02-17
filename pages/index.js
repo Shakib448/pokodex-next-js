6086db67dfb11b55507b97e9f9c2e54fcff870bf
Shakib448 <shakiba448@gmail.com>
2021-02-17 15:14:37 +0600

    NEXT JS

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,20 @@ const Home = ({ pokemon }) => {
       <ul>
         {pokemon.map((pokeman, index) => (
           <li key={index}>
-            <Link href={`/pokemon?id=${index + 1}`}></Link>
+            <Link href={`/pokemon?id=${index + 1}`}>
+              <a className="border p-4 border-gray my-2 capitalize flex items-center text-lg bg-gray-200 rounded-md">
+                <img
+                  className="w-20 h-20 mr-3"
+                  src={pokeman.image}
+                  alt={pokeman.name}
+                />
+
+                <span className="mr-2 font-bold">
+                  {" "}
+                  {index + 1} {pokeman.name}
+                </span>
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
