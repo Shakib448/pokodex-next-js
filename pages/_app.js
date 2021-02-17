ad6874c7b465c0f883b0a1309eb3e198ed81d7a9
Shakib448 <shakiba448@gmail.com>
2021-02-17 19:50:02 +0600

    WIP on STYLE

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
 import "../styles/globals.css";
 import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css"; //styles of nprogress
-//Binding events.
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/Theme/theme";
+import React, { useEffect } from "react";
+
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
