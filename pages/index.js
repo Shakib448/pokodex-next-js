44f4588f32fba8ef63a2d54389359716fd793187
Shakib448 <shakiba448@gmail.com>
2021-02-17 15:01:58 +0600

    NEXT JS

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Layout from "../Components/Layout";
 
 const Home = () => {
@@ -8,10 +9,32 @@ const Home = () => {
   );
 };
 
-export async function getStaticProps(context) {
+export const getStaticProps = async (context) => {
+  try {
+    const {
+      data: { results },
+    } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=150");
+    const pokemon = results.map((result, index) => {
+      const paddedIndex = ("00" + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return { ...result, image };
+    });
+  } catch (error) {
+    console.error(error);
+  }
   return {
-    props: {},
+    props: { pokemon },
   };
-}
+};
 
+// export async function getStaticProps(context) {
+//   try {
+//     const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+//     const { results } = await res.json();
+//     console.log(results);
+//   } catch (error) {}
+//   return {
+//     props: {},
+//   };
+// }
 export default Home;
