70fcfbde1d560957d6fc1a4c186a2bbc5d00e9bc
Shakib448 <shakiba448@gmail.com>
2021-02-17 14:30:32 +0600

    NEXT JS

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,15 @@ import Layout from "../Components/Layout";
 const Home = () => {
   return (
     <Layout title="POKODEX">
-      <h1> NEXT JS Pokodex</h1>
+      <h1 className="text-4xl mb-8 text-center"> NEXT JS Pokedex</h1>
     </Layout>
   );
 };
+
+export async function getStaticProps(context) {
+  return {
+    props: {},
+  };
+}
 
 export default Home;
